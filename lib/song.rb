12e4ba3f243a999a27f06e5c54cf8f9a51008f9e
lavikoder <kevkamanda@gmail.com>
2020-01-24 19:28:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,55 @@
+require 'pry'
+
 class Song
+@@count = 0
+@@artists = []
+@@genres = []
+attr_accessor :name, :artist, :genre
 
-  attr_accessor :name, :artist, :genre
-
-  def initailize(name, artist, genre)
-    @song = song
+  def initialize(name, artist, genre)
+    @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
   end
 
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    hash = {}
+    @@genres.each do |x|
+      if hash.keys.include?(x)
+        hash[x] += 1
+      else
+        hash[x] = 1
+        # binding.pry
+      end
+    end
+    hash
+  end
+
+  def self.artist_count
+    hash = {}
+    @@artists.each do |x|
+      if hash.keys.include?(x)
+        hash[x] += 1
+      else
+        hash[x] = 1
+        # binding.pry
+      end
+    end
+    hash
+  end
 end
